09f5fa9283a44b6832d7ed8a4e7f2ff9625d8bd5
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2024-07-05 03:02:48 +0600

    added gameboard grid

--- React-Tic-Tac-Toe/src/App.jsx
+++ React-Tic-Tac-Toe/src/App.jsx
@@ -1,7 +1,7 @@
 import Player from "./components/Player"
+import GameBoard from "./components/GameBoard"
 
 function App() {
-  
   return (
     <main>
       <div id="game-container">
@@ -9,7 +9,7 @@ function App() {
           <Player initialName="Player 1" symbol="X" />
           <Player initialName="Player 2" symbol="0" />
         </ol>
-
+        <GameBoard />
       </div>
     </main>
   )
