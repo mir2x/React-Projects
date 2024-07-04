3f3bbdb61da6e2fdb896aadbaeb3a9f1550f704d
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2024-07-05 03:09:29 +0600

    adding symbol click

--- React-Tic-Tac-Toe/src/components/GameBoard.jsx
+++ React-Tic-Tac-Toe/src/components/GameBoard.jsx
@@ -1,17 +1,29 @@
+import { useState } from "react"
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null]
 ]
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard)
+
+  function handleClick(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedGameBoard = [...prevGameBoard]
+      updatedGameBoard[rowIndex][colIndex] = 'X'
+      return updatedGameBoard
+    })
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((symbol, colIndex) => (
               <li key={colIndex}>
-                <button>{symbol}</button>
+                <button onClick={() => handleClick(rowIndex, colIndex)}>{symbol}</button>
               </li>
             ))}
           </ol>
