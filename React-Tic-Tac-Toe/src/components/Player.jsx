9d5fcbf654030950a6249c97a607611132f8ba68
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2024-07-05 03:02:30 +0600

    updated code

--- React-Tic-Tac-Toe/src/components/Player.jsx
+++ React-Tic-Tac-Toe/src/components/Player.jsx
@@ -2,29 +2,29 @@ import { useState } from "react"
 
 export default function Player({initialName, symbol}) {
     
-    const [name, setName] = useState(initialName)
-    const [isEditing, setIsEditing] = useState(false)
+  const [name, setName] = useState(initialName)
+  const [isEditing, setIsEditing] = useState(false)
 
-    function handleClick() {
-        setIsEditing((editing) => !editing)
-    }
+  function handleClick() {
+      setIsEditing((editing) => !editing)
+  }
 
-    function handleChange(event) {
-        setName(event.target.value)
-    }
+  function handleChange(event) {
+      setName(event.target.value)
+  }
 
-    let nameComponent = <span className="player-name">{name}</span>
-    if(isEditing) {
-        nameComponent = <input type="text" required value={name} onChange={handleChange} />
-    }
+  let nameComponent = <span className="player-name">{name}</span>
+  if(isEditing) {
+      nameComponent = <input type="text" required value={name} onChange={handleChange} />
+  }
 
-    return (
-        <li>
-            <span className="player">
-                {nameComponent}
-                <span className="player-symbol">{symbol}</span>
-            </span>
-            <button onClick={handleClick}>{(isEditing) ? "Save" : "Edit"}</button> 
-        </li>
-    )
+  return (
+    <li>
+      <span className="player">
+        {nameComponent}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleClick}>{(isEditing) ? "Save" : "Edit"}</button> 
+      </li>
+  )
 }
