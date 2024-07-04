b7582f6448f25bcee853bb609abadd8d454c3618
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2024-07-05 02:28:23 +0600

    added player component

---
+++ React-Tic-Tac-Toe/src/components/Player.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+export default function Player({initialName, symbol}) {
+    
+    const [name, setName] = useState(initialName)
+    const [isEditing, setIsEditing] = useState(false)
+
+    function handleClick() {
+        setIsEditing((editing) => !editing)
+    }
+
+    function handleChange(event) {
+        setName(event.target.value)
+    }
+
+    let nameComponent = <span className="player-name">{name}</span>
+    if(isEditing) {
+        nameComponent = <input type="text" required value={name} onChange={handleChange} />
+    }
+
+    return (
+        <li>
+            <span className="player">
+                {nameComponent}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleClick}>{(isEditing) ? "Save" : "Edit"}</button> 
+        </li>
+    )
+}
