d696d097f1322a7e14561b2e6953095b2a7bd83f
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2024-07-05 01:00:57 +0600

    Forwarding props to nested components

--- React-Essentials/src/components/TabButton.jsx
+++ React-Essentials/src/components/TabButton.jsx
@@ -1,5 +1,5 @@
-export default function TabButton({children, isSelected, onSelect}) {
+export default function TabButton({children, isSelected, ...props}) {
     return (
-        <button className={isSelected ? "active":undefined} onClick={onSelect}>{children}</button>
+        <button className={isSelected ? "active":undefined} {...props}>{children}</button>
     );
 }
